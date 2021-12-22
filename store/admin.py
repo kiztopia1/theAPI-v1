f37d7bee914bef38a7265e5d2586f4764d001bf2
kirubel <kiztopia.2020@gmail.com>
2021-12-22 09:59:46 +0300

    solditem model added and rigisterd

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Item, soldItems
 # Register your models here.
+
+
+admin.site.register(Item)
+admin.site.register(soldItems)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    sellingPrice = models.CharField(max_length=100)
+    amount = models.CharField(max_length=100)
+    expDate = models.DateField()
+
+
+    def __str__(self):
+        return self.name
+
+
+class soldItems(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    amount = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.item.name
