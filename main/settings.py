674d52ed25f54d7015f225934cbb290c91e26f05
kirubel <kiztopia.2020@gmail.com>
2021-12-27 11:08:13 +0300

    cors module added

--- main/settings.py
+++ main/settings.py
@@ -39,10 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
     'rest_framework',
+    'corsheaders',
 
 ]
 
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:3000",
+    "http://127.0.0.1:9000"
+] 
+CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
