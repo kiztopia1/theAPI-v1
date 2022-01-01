2759616a77cea2640c5e04bf59489a51341e0e80
kirubel <kiztopia.2020@gmail.com>
2022-01-01 15:17:49 +0300

    adding url paths coment

--- store/urls.py
+++ store/urls.py
@@ -3,9 +3,14 @@ from django.urls import path
 from .apiViews import ItemCreate, ReteriveUpdateDestroy, SoldItemCreate,soldItemsList,ItemsList
 
 urlpatterns = [
-    path('', ItemCreate.as_view()),
+    # add new item
+    path('item/add', ItemCreate.as_view()),
+    # list of items
     path('items/', ItemsList.as_view()),
-    path('<int:key>', ReteriveUpdateDestroy.as_view()),
-    path('sold/', soldItemsList.as_view()),
+    # items edit delete update
+    path('item/<int:key>', ReteriveUpdateDestroy.as_view()),
+    #list of sold items
+    path('sold-items/', soldItemsList.as_view()),
+    # add new sold Item
     path('sold/add', SoldItemCreate.as_view()),
 ]
