e307ee98088b218deacf3d675c7e700eaa8a3606
kirubel <kiztopia.2020@gmail.com>
2022-02-14 05:14:23 +0300

    new: font size changed to 14 px for all smal texts

--- store/forms.py
+++ store/forms.py
@@ -7,9 +7,9 @@ class ItemForm (forms.ModelForm):
         fields = ('__all__')
         
         widgets = {
-            'key': forms.TextInput(attrs={'class': 'form-control'}),
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'price': forms.TextInput(attrs={'class': 'form-control'}),
+            'key': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'name': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'price': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),
             'alert': forms.CheckboxInput(attrs={'class': ''}),
 
         }
