f938159836944b91d06843bfda7a42d6cb94e948
kirubel <kiztopia.2020@gmail.com>
2022-02-15 19:06:27 +0300

    new: proucts model updated

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Item, SoldItem, Product
+from .models import Supplier, Brand, Tax, Category, Product
 # Register your models here.
 
 
-admin.site.register(Item)
+admin.site.register(Tax)
 admin.site.register(Product)
+admin.site.register(Supplier)
+admin.site.register(Brand)
+admin.site.register(Category)
+
